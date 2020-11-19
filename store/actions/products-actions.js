d07088c6120f8e1968f3f83ea50c346471a1c12f
Julie <julienne.bay@gmail.com>
2020-11-19 17:33:59 -0500

    set up Firebase to send requests and store the product info on its servers. Right now, the app is storing the newly created products on a server, but it's not fetching them from a server.

--- store/actions/products-actions.js
+++ store/actions/products-actions.js
@@ -7,14 +7,40 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    //you can write any async code you want here (thanks to REDUX THUNK)
+    //that URL below is from my Firebase...must add ".json" (that 'products' was added by me - it'll be saved as a seperate folder in firebase )
+    //fetch API is built in (in React Native)
+    const response = await fetch(
+      "https://rn-shopping-app-69186.firebaseio.com/products.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price
+        })
+      }
+    );
+
+    const resData = await response.json();
+
+    console.log(resData);
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
   };
 };
 

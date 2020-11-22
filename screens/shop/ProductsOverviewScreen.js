ef9b6ee53b8e77b121f5fab9d8abf0871639e9cf
Julie <julienne.bay@gmail.com>
2020-11-21 20:55:31 -0500

    The app now fetches products from the server (set up w/ firebase), and it can update/delete products as well. I've also added a loading spinner & an error message

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,18 +1,56 @@
-import React from "react";
-import { FlatList, Button } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  ActivityIndicator,
+  StyleSheet
+} from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
 import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from "../../store/actions/cart-actions";
+import * as productsActions from "../../store/actions/products-actions";
 import Colors from "../../constants/Colors";
 
 const ProductsOverviewScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   //the key "products" in state.products below has to match the key in the rootReducer in App.js
   //and the key "availableProducts" is the key from ../store/reducers/products.
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
+
+  const loadProducts = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    try {
+      await dispatch(productsActions.fetchProducts());
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  //navigation listener - lecture 204
+  //whenever the user goes back to this screen, it re-fetches the screen
+  useEffect(() => {
+    const willFocusSub = props.navigation.addListener(
+      "willFocus",
+      loadProducts
+    );
+
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [loadProducts]);
+
+  useEffect(() => {
+    loadProducts();
+  }, [dispatch, loadProducts]);
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate({
@@ -24,10 +62,40 @@ const ProductsOverviewScreen = props => {
     });
   };
 
+  if (error) {
+    return (
+      <View style={styles.spinner}>
+        <Text>An error has occurred!</Text>
+        <Button
+          title="Try again"
+          onPress={loadProducts}
+          color={Colors.primary}
+        />
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      //activityIndicator = built in "loading spinner"
+      <View style={styles.spinner}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.spinner}>
+        <Text>No products found. You can add some!</Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={products}
-      //the keys below MUST MATCH the keys inside ../../models/produdct
+      //the keys below MUST MATCH the keys inside ../../models/product
       renderItem={itemData => (
         <ProductItem
           image={itemData.item.imageUrl}
@@ -87,5 +155,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  spinner: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default ProductsOverviewScreen;
--- store/actions/products-actions.js
+++ store/actions/products-actions.js
@@ -1,9 +1,59 @@
+import Product from "../../models/product";
+
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const SET_PRODUCTS = "SET_PRODUCTS";
+//set products has no identifier, because we'll never dispatch it as an action, which will reach a reducer
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    try {
+      //you can write any async code you want here (thanks to REDUX THUNK)
+      //that URL below is from my Firebase...must add ".json" (that 'products' was added by me - it'll be saved as a seperate folder in firebase )
+      //fetch API is built in (in React Native)
+      const response = await fetch(
+        "https://rn-shopping-app-69186.firebaseio.com/products.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Error - something went wrong");
+      }
+
+      const resData = await response.json();
+      //we'll receive an object back as our data, so we need to translate that into an array (which we use)
+      const loadedProducts = [];
+      //the key is the unique ID given by firebase
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (error) {
+      //you can do more here, like send to customs analytics server
+      throw error;
+    }
+  };
+};
 
 export const deleteProduct = productId => {
-  return { type: DELETE_PRODUCT, pId: productId };
+  return async dispatch => {
+    await fetch(
+      `https://rn-shopping-app-69186.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE"
+      }
+    );
+    dispatch({ type: DELETE_PRODUCT, pId: productId });
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -29,7 +79,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
     const resData = await response.json();
 
-    console.log(resData);
+    // console.log(resData);
 
     dispatch({
       type: CREATE_PRODUCT,
@@ -45,13 +95,30 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pId: id,
-    productData: {
-      title,
-      description,
-      imageUrl
-    }
+  return async dispatch => {
+    await fetch(
+      `https://rn-shopping-app-69186.firebaseio.com/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl
+        })
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pId: id,
+      productData: {
+        title,
+        description,
+        imageUrl
+      }
+    });
   };
 };
